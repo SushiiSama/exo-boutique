0130b6a08bec45fe786bece00a235395785d5671
SushiiSama <metaleux-kira@hotmail.fr>
2022-09-02 08:39:19 +0100

    2 eme étape

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -5,7 +5,7 @@ const CategoryCard = () => {
     return (
       <>
        <div className="card container " >
-  <img src="https://picsum.photos/200/100" className="card-img-top " alt="..."/>
+  <img src="https://picsum.photos/200/100" className="card-img-top *" alt="..."/>
   <div className="card-body ">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
