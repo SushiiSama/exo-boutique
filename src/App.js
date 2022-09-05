ebcbdb1c7dd69c97fdf8ebd1e86761552f8bfa3d
SushiiSama <metaleux-kira@hotmail.fr>
2022-09-05 14:26:02 +0100

    etape 6 /7 terminé

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import React from "react";
 import "./App.css";
 import CategoryCard from "./components/CategoryCard";
 import ContactForm from "./components/ContactForm";
 import HomeScreen from "./pages/HomeScreen";
 
+
+
+
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+  if(darkMode){
+  document.body.classList.add("dark");
+} else{
+document.body.classList.remove("dark");
+}
+},[darkMode]);
+
+
   return (
     <>
       <div class="container-fluid">
         <HomeScreen />
         <ContactForm />
+          <button onClick={() => setDarkMode(!darkMode)} >toggle dark mode </button>
         <div class="row"></div>
       </div>
     </>
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 
 const ContactForm = () => {
-  return (
-    <div>
 
-         <h2  className="d-flex justify-content-center">Contact Me</h2>
-      <input
-        className="form-control form-control-lg bg-dark m-3"
-        type="text"
-        placeholder=".form-control-lg"
-      />
-      <input className="form-control bg-dark m-3" type="text" placeholder="Default input" />
-      <input
-        className="form-control form-control-sm m-3 bg-dark"
-        type="text"
-        placeholder=".form-control-sm"
-      />
-      <button className="p-3   d-flex ms-auto  bg-danger " type="input"> Button</button>
-    </div>
+   const HandleSubmit =(evt) => {
+     evt.preventDefault();
+     const form  = evt.target ;
+     const formData = new FormData(form);
+     const jsonData = Object.fromEntries(formData.entries())
+     console.log(jsonData)
+
+
+}
+   
+
+  return (
+    <>
+    
+    <form className="p-3" onSubmit={HandleSubmit}>
+  <div className="form-group">
+    <label HTMLFor="exampleInputEmail1">Email address</label>
+    <input type="email" name ="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <label HTMLFor="exampleInputPassword1">Password</label>
+    <input type="password" name =" mdp" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" name="check" className="form-check-input" id="exampleCheck1"  />
+    <label className="form-check-label" HTMLFor="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+    
+    </>
   );
 };
 
